a3225778ef36f21f7f45952b0aa253d2aa3c355d
Davy'd <pdaswift3009@gmail.com>
2018-01-06 13:08:57 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,45 +1,96 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var session = require('express-session')
+var flash = require('express-flash')
+var ejs   = require('ejs');
+var ejs_mate = require('ejs-mate');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
+
+var secret = require('./config/secret')
+//require models
+var User = require('./models/user');
+
+//require routes
 var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
+
+// Connect to database
+const db = mongoose.connect(secret.database, function(err){
+  (err)? console.log(err): console.log("Build something Amazing!!!");
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejs_mate)
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(cookieParser());
+app.use(session({
+    secret: secret.secretKey,
+    resave: true,
+    saveUninitialized: true,
+    store : new MongoStore({url : secret.database, autoReconnect : true})
+}));
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport.use(User.createStrategy());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+app.use(flash());
+
+// middleware
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
+});
+
+
+
+
+app.use('/', index);
+app.use('/', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router =express.Router();
+var passport = require('passport');
+var passportConf = require('../config/passport');
+var User = require('../models/user');
+
+
+
+router.get('/login', function(req, res){
+  if(req.user) return res.redirect('/');
+  res.render('profiles/login', {message : req.flash('loginMessage')});
+});
+
+
+
+router.post('/login', passport.authenticate('local-login',{
+   successRedirect : '/profile',
+   failureRedirect: '/login',
+   failureFlash : true
+}));
+
+
+router.get('/profile', isLoggedIn, function(req, res){
+   User.findOne({ _id : req.user._id}, function(err, user){
+     if(err) return next(err);
+    res.render('profiles/profile', {user : user});
+  });
+});
+
+
+
+/* GET users listing. */
+router.get('/register', function(req, res,next){
+   res.render('profiles/register', {error: req.flash('error')});
+})
+
+
+
+/* POST users listing. */
+router.post('/register', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  if(req.body.email.length && req.body.name.length > 0 ){
+     User.findOne({email : req.body.email}, function(err, userExist){
+      if(userExist){
+         req.flash('error', 'User with the provided email address already exist!!!');
+         return res.redirect('register');
+      }
+      user.save(function (err, user){
+           if(err) return next(err);
+
+           req.logIn(user, function(err){
+            if(err) return next(err);
+            res.redirect('/profile');
+           });
+        });
+    })
+  }else{
+    req.flash('error', 'Make sure you are filling the right information');
+    return res.redirect('register');
+  }
+});
+
+
+
+
+// LOGOUT ROUTE
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', "Goodbye")
+  res.redirect('/')
+});
+
+
+
+// MIDDLEWARE
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  res.redirect('/login');
+}
+
+
+
+module.exports = router;
